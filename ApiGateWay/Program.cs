f95ab5b697b6c52bb82e210fad7acd52819695cb
Fathi <ahmedmhmd0237@gmail.com>
2025-05-05 01:30:32 +0200

    Add RateLimiting to All MicroServices

--- ApiGateWay/Program.cs
+++ ApiGateWay/Program.cs
@@ -1,35 +1,46 @@
 ﻿using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using AspNetCoreRateLimit;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// ✅ تحميل إعدادات Ocelot
+
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
-// ✅ تسجيل خدمات Ocelot
+
 builder.Services.AddOcelot();
 
-// Add services to the container.
+builder.Services.AddMemoryCache();
+builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
+builder.Services.Configure<IpRateLimitPolicies>(builder.Configuration.GetSection("IpRateLimitPolicies"));
+builder.Services.AddInMemoryRateLimiting();
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseIpRateLimiting(); 
+
+app.UseHttpsRedirection();  
+app.UseAuthorization();     
+
 
 app.MapControllers();
 
-// ✅ تفعيل Ocelot
+
 await app.UseOcelot();
+
 
 app.Run();
